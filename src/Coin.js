7189adf56657bc76847286b2219681336ad26019
Matthew Phillips <phillips.matthew829@gmail.com>
2022-02-11 12:40:26 -0600

    Add functionality to delete coin Object

--- src/Coin.js
+++ src/Coin.js
@@ -1,9 +1,14 @@
+import React, {useContext} from "react";
+import {CoinInvContext} from "./context/coinInv";
+
 // Need to implement:
 // Create "Edit" and "Delete" Buttons with functions
 
 // Write the Array of coin Objects to db.json
 
 function Coin({coin}){
+    const {coinInv, setCoinInv} = useContext(CoinInvContext);
+
     console.log(coin);
 
     function updateCoin(){ // Function to update a selected coin Object
@@ -11,12 +16,16 @@ function Coin({coin}){
     }
 
     function deleteCoin(){ // Function to delete a coin Object
-        /* fetch(`<url>/${coin.id}`, { // 
+    fetch(`http://localhost:3001/coins/${coin.id}`, { // DELETE fetch request
       method: "DELETE",
     })
     .then((r) => r.json())
-    .then(() => onDeleteCoin(coin)); //
-    */
+    .then(() => onDeleteCoin(coin)); // Invoke the onDeleteCoin function with this fetch request
+    }
+
+    function onDeleteCoin(deletedCoin){ //Function 
+        const updatedCoins = coinInv.filter((coin) => coin.id !== deletedCoin.id);
+    setCoinInv(updatedCoins);
     }
 
     return(
